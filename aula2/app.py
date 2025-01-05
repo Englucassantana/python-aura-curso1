f65980bdb35dba3c4206e1ba1aa18ddc18e2a573
Lucas Santana <eng.lucassantana@gmail.com>
2025-01-05 19:31:56 -0300

    feat: faça como fiz aula dois

--- aula2/app.py
+++ aula2/app.py
@@ -1,26 +1,35 @@
-print(""""
-░██████╗░█████╗░██████╗░░█████╗░██████╗░  ███████╗██╗░░██╗██████╗░██████╗░███████╗███████╗░██████╗░██████╗
-██╔════╝██╔══██╗██╔══██╗██╔══██╗██╔══██╗  ██╔════╝╚██╗██╔╝██╔══██╗██╔══██╗██╔════╝██╔════╝██╔════╝██╔════╝
-╚█████╗░███████║██████╦╝██║░░██║██████╔╝  █████╗░░░╚███╔╝░██████╔╝██████╔╝█████╗░░█████╗░░╚█████╗░╚█████╗░
-░╚═══██╗██╔══██║██╔══██╗██║░░██║██╔══██╗  ██╔══╝░░░██╔██╗░██╔═══╝░██╔══██╗██╔══╝░░██╔══╝░░░╚═══██╗░╚═══██╗
-██████╔╝██║░░██║██████╦╝╚█████╔╝██║░░██║  ███████╗██╔╝╚██╗██║░░░░░██║░░██║███████╗███████╗██████╔╝██████╔╝
-╚═════╝░╚═╝░░╚═╝╚═════╝░░╚════╝░╚═╝░░╚═╝  ╚══════╝╚═╝░░╚═╝╚═╝░░░░░╚═╝░░╚═╝╚══════╝╚══════╝╚═════╝░╚═════╝░
-""")
-
-print('1. Cadastrar restaurante')
-print('2. Listar restaurantes')
-print('3. Ativar restaurante')
-print('4. Sair\n')
-
-opcao_escolhida = int(input('Escolha uma opção: '))
-
-print(f'Opção escolhida {opcao_escolhida}!')
-
-if opcao_escolhida == 1:
-  print('Cadastrando restaurante')
-elif opcao_escolhida == 2:
-  print('Listando restarantes')
-elif opcao_escolhida == 3:
-  print('Ativando restaurante')
-else :
+import os
+def show_program_name():
+  print(""""
+  ░██████╗░█████╗░██████╗░░█████╗░██████╗░  ███████╗██╗░░██╗██████╗░██████╗░███████╗███████╗░██████╗░██████╗
+  ██╔════╝██╔══██╗██╔══██╗██╔══██╗██╔══██╗  ██╔════╝╚██╗██╔╝██╔══██╗██╔══██╗██╔════╝██╔════╝██╔════╝██╔════╝
+  ╚█████╗░███████║██████╦╝██║░░██║██████╔╝  █████╗░░░╚███╔╝░██████╔╝██████╔╝█████╗░░█████╗░░╚█████╗░╚█████╗░
+  ░╚═══██╗██╔══██║██╔══██╗██║░░██║██╔══██╗  ██╔══╝░░░██╔██╗░██╔═══╝░██╔══██╗██╔══╝░░██╔══╝░░░╚═══██╗░╚═══██╗
+  ██████╔╝██║░░██║██████╦╝╚█████╔╝██║░░██║  ███████╗██╔╝╚██╗██║░░░░░██║░░██║███████╗███████╗██████╔╝██████╔╝
+  ╚═════╝░╚═╝░░╚═╝╚═════╝░░╚════╝░╚═╝░░╚═╝  ╚══════╝╚═╝░░╚═╝╚═╝░░░░░╚═╝░░╚═╝╚══════╝╚══════╝╚═════╝░╚═════╝░
+  """)
+def show_options():
+  print('1. Cadastrar restaurante')
+  print('2. Listar restaurantes')
+  print('3. Ativar restaurante')
+  print('4. Sair\n')
+def encerrando_aplicação():
+  os.system('clear') 
   print('Encerrando aplicação')
+def choose_option():
+  opcao_escolhida = int(input('Escolha uma opção: '))
+  print(f'Opção escolhida {opcao_escolhida}!')
+  if opcao_escolhida == 1:
+    print('Cadastrando restaurante')
+  elif opcao_escolhida == 2:
+    print('Listando restarantes')
+  elif opcao_escolhida == 3:
+    print('Ativando restaurante')
+  else :
+    encerrando_aplicação()
+def main():
+  show_program_name();
+  show_options();
+  choose_option();
+if __name__ == '__main__':
+  main()
