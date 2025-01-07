f388cd1fc4ce83021d716a2b4bdf3a4b7cb6d734
Lucas Santana <eng.lucassantana@gmail.com>
2025-01-07 17:18:41 -0300

    feat: usar match

--- aula2/app.py
+++ aula2/app.py
@@ -19,14 +19,23 @@ def encerrando_aplicação():
 def choose_option():
   opcao_escolhida = int(input('Escolha uma opção: '))
   print(f'Opção escolhida {opcao_escolhida}!')
-  if opcao_escolhida == 1:
-    print('Cadastrando restaurante')
-  elif opcao_escolhida == 2:
-    print('Listando restarantes')
-  elif opcao_escolhida == 3:
-    print('Ativando restaurante')
-  else :
-    encerrando_aplicação()
+  # if opcao_escolhida == 1:
+  #   print('Cadastrando restaurante')
+  # elif opcao_escolhida == 2:
+  #   print('Listando restarantes')
+  # elif opcao_escolhida == 3:
+  #   print('Ativando restaurante')
+  # else :
+  #   encerrando_aplicação()
+  match opcao_escolhida:
+    case 1:
+      print('Cadastrando restaurante')
+    case 2:
+      print('Listando restarantes')
+    case 3:
+      print('Ativando restaurante')
+    case _:
+      encerrando_aplicação()
 def main():
   show_program_name();
   show_options();
